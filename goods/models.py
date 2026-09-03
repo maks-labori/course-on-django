9cf8573ede8b1577c66cb1f847a4a28463113c8b
maksuta <dartmakstlt@gmail.com>
2026-09-03 23:19:11 +0300

    work with migrate

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class categories(models.Model):
+    name = models.CharField(max_length=150,unique=True,verbose_name='Название')
+    slug = models.SlugField(max_length=200,unique=True,blank=True,null=True,verbose_name='Путь')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
